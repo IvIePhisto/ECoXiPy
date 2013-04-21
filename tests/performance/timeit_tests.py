baaca73589fbcaa7d1ea288344dd5164c7f6cbe8
Michael Pohl <pohl-michael@gmx.net>
2013-04-21 17:10:30 +0200

    better timeit test output

--- tests/performance/timeit_tests.py
+++ tests/performance/timeit_tests.py
@@ -7,13 +7,12 @@ import ecoxipy_string_output
 import ecoxipy_dom_output
 
 
-TIMEIT_NUMBER = 200
-TIMEIT_DATA_COUNT = 100
+TIMEIT_NUMBER = 50
+TIMEIT_DATA_COUNT = 50
 
 if __name__ == '__main__':
     create_test_run = lambda module: "{}.create_testdoc('Test Page', 'Hello World!', {})".format(module.__name__, TIMEIT_DATA_COUNT)
     create_test_setup = lambda module: "from __main__ import {0}; {0}.create_testdoc('startup', 'startup', {1})".format(module.__name__, TIMEIT_DATA_COUNT)
-    create_percent = lambda t, m: '{: >6.2f} %'.format((t-m)/m*100)
     timeit_run = lambda module: timeit.timeit(
         create_test_run(module),
         setup=create_test_setup(module),
@@ -25,28 +24,30 @@ if __name__ == '__main__':
     dom_out_time = timeit_run(ecoxipy_dom_output)
     min_time = min(sax_time, dom_time, element_out_time, string_out_time, dom_out_time)
     max_time = max(sax_time, dom_time, element_out_time, string_out_time, dom_out_time)
-
+    create_percent = lambda t: '| {: >6.2f} % | {: >6.3f} secs |'.format((t-min_time)/(max_time-min_time)*100, t)
     print '''
-ECoXiPy Performance Tests
-=========================
+# ECoXiPy Performance Tests
 
-Number of repetitions:      {}
-Number of data elements:    {}
+Number of repetitions:   {}
+Number of data elements: {}
 
-Minimum Time:           {}
-Maximum Time:           {}
+## Run Time Results
 
-The individual APIs had a runtime increase in respect to the minimum time of:
-xml.sax                 {}
-xml.dom.minidom         {}
-ecoxipy.dom_output      {}
-ecoxipy.element_output  {}
-ecoxipy.string_output   {}
+Minimum: {: >6.3f} secs
+Maximum: {: >6.3f} secs
+
+|API                    | relative | absolute    |
+|-----------------------|----------|-------------|
+|xml.sax                {}
+|xml.dom.minidom        {}
+|ecoxipy.dom_output     {}
+|ecoxipy.element_output {}
+|ecoxipy.string_output  {}
 '''.format(
         TIMEIT_NUMBER, TIMEIT_DATA_COUNT, min_time, max_time,
-        create_percent(sax_time, min_time),
-        create_percent(dom_time, min_time),
-        create_percent(dom_out_time, min_time),
-        create_percent(element_out_time, min_time),
-        create_percent(string_out_time, min_time),
+        create_percent(sax_time),
+        create_percent(dom_time),
+        create_percent(dom_out_time),
+        create_percent(element_out_time),
+        create_percent(string_out_time),
     )
